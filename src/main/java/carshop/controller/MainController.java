7a1b19e316ec2ffbea5fd686bd253f06da5f5007
Petko Georgiev <petko.vas.georgiev@gmail.com>
2017-04-25 22:56:37 +0300

    Add SecurityConfig and MainController

---
+++ src/main/java/carshop/controller/MainController.java
@@ -0,0 +1,37 @@
+package carshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String postLogin() {
+        System.out.println("HEy, theereeee");
+        return "redirect:/login-error";
+    }
+
+    @RequestMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login";
+    }
+}
